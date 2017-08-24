cce37b2eccadea0f09f5894f5cb39e5e1717307c
J.Heinen <j.heinen@eurokey.de>
2017-08-24 10:48:57 +0200

    fixed view bugs

--- SPM/Models/AccountViewModels/RegisterViewModel.cs
+++ SPM/Models/AccountViewModels/RegisterViewModel.cs
@@ -14,12 +14,10 @@ namespace SPM.Models.AccountViewModels
         public string Username { get; set; }
 
         [Required]
-        [StringLength(24, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
-        [StringLength(24, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
